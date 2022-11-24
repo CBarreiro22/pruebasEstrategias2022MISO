6e0a5b53b016f8fe00213f8feb0f5a247a282fe8
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2022-11-24 06:45:37 -0500

    add scenaries member with number

---
+++ cypress/e2e/PruebasAleatorias/EscenarioMiembroEmailErroneoNumero/sinNombreEmailErroneoNumero.js
@@ -0,0 +1,13 @@
+import {faker} from '@faker-js/faker'
+class SinNombreEmailErroneoNumero {
+    SinNombreEmailErroneoNumeroMiembro() {
+        let content=faker.random.numeric(12)
+        cy.get('input[name="email"]').type(content)
+        cy.get('input[name="email"]').should('not.have.value','@gmail.com')               
+        cy.wait(1000)
+        cy.get('button[class="gh-btn gh-btn-primary gh-btn-icon ember-view"]').contains('span','Save').click()
+        cy.wait(1000)
+    }
+
+}
+export default SinNombreEmailErroneoNumero
