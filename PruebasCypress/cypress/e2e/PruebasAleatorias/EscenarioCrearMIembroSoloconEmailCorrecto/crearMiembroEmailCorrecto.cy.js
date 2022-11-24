6ff408ac33ece97381d81d4f04c1e6757772fc82
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2022-11-23 20:47:36 -0500

    update

---
+++ PruebasCypress/cypress/e2e/PruebasAleatorias/EscenarioCrearMIembroSoloconEmailCorrecto/crearMiembroEmailCorrecto.cy.js
@@ -0,0 +1,35 @@
+import Login from "../login";
+import ingresoMiembro from "../ingresoMiembro";
+import SoloEmailCorrecto from "./soloEmailCorrecto";
+
+const email = Cypress.env('email')
+const password = Cypress.env('password')
+
+describe('Testing basic Ghost', () => {
+    context('Given I access the search engine page', () => {
+        beforeEach(()=>{
+            const login = new Login();
+            login.navigate();
+        })
+        context('When I write and login in the page', () => {
+            beforeEach(()=>{
+                const login = new Login();
+                login.enterEmail(email);
+                login.enterPassword(password);
+                cy.wait(1000)
+                cy.get('input[name="identification"]').should('have.value',email)
+                cy.wait(1000)
+                cy.get('input[name="password"]').should('have.value',password)
+                cy.wait(2000)
+                login.submit();
+            });
+ 
+            it("Then the title and content should be empty", function () {
+                const ingreso = new ingresoMiembro();
+                const soloEmailCorrecto = new SoloEmailCorrecto();
+                ingreso.ingresoMiembro();
+                soloEmailCorrecto.soloEmailCorrectoMiembro();
+            })
+        })
+    })
+})
---
+++ PruebasCypress/cypress/e2e/PruebasAleatorias/EscenariopostTituloyContenidomas250caracteres /crearPostContenidomas250caracteres.cy.js
@@ -0,0 +1,36 @@
+import Login from "../login";
+import IngresoPost from "../ingresoPost";
+import TituloyContenidomas250caracteres from "./tituloyContenidomas250caracteres";
+
+
+const email = Cypress.env('email')
+const password = Cypress.env('password')
+
+describe('Testing basic Ghost', () => {
+    context('Given I access the search engine page', () => {
+        beforeEach(()=>{
+            const login = new Login();
+            login.navigate();
+        })
+        context('When I write and login in the page', () => {
+            beforeEach(()=>{
+                const login = new Login();
+                login.enterEmail(email);
+                login.enterPassword(password);
+                cy.wait(1000)
+                cy.get('input[name="identification"]').should('have.value',email)
+                cy.wait(1000)
+                cy.get('input[name="password"]').should('have.value',password)
+                cy.wait(2000)
+                login.submit();
+            });
+ 
+            it("Then content and title should not be empty", function () {
+                const ingreso = new IngresoPost();
+                const tituloContenidoPost = new TituloyContenidomas250caracteres();
+                ingreso.ingresoPost();
+                tituloContenidoPost.tituloyContenido250Post();
+            })
+        })
+    })
+})
