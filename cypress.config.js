413a3dd4ed161ca0c3edcb6354560fede43636b0
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2022-11-24 04:22:01 -0500

    update

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:2368/ghost/#/signin'
+  },
+  env: {
+    email: '',
+    password: ''
+  }
+})
