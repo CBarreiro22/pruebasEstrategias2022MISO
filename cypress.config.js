28defc4aad57df65fdee0c611a3beef97da011c4
Isai Oliva <i.oliva@uniandes.edu.co>
2022-11-24 21:33:58 -0400

    estructuración de carpetas

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:2368/ghost/#/signin'
+    baseUrl: 'http://localhost:3001/ghost/#/signin'
   },
   env: {
-    email: 'c.barreiroh@uniandes.edu.co',
-    password: 'Q123456789'
+    email: 'e.angulov@uniandes.edu.co',
+    password: 'Elymary-420@'
   }
 })
