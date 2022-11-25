aefc690e47dd82dfa3af5f813dbea28cd0653f83
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2022-11-25 05:53:22 -0500

    scenario miembro nombnre mas 191

---
+++ cypress/e2e/PruebasAleatorias/EscenarioCrearMiembroEmailyNombreMas191/crearMiembroEmailNombreMas191.js
@@ -0,0 +1,19 @@
+import {faker} from '@faker-js/faker'
+class CrearMiembroEmailNombreMas191 {
+    crearMiembroEmailNombreMas191() {
+        let content=faker.internet.email()
+        let content2 = faker.random.alphaNumeric(192)
+        //Name
+        cy.get('input[name="name"]').type(content2)
+        //validation
+        cy.get('input[name="name"]').should('have.value',content2)
+        //Email
+        cy.get('input[name="email"]').type(content)
+        //validation
+        cy.get('input[name="email"]').should('have.value',content)  
+        cy.get('button[class="gh-btn gh-btn-primary gh-btn-icon ember-view"]').contains('span','Save').click()
+        cy.wait(1000)
+    }
+
+}
+export default CrearMiembroEmailNombreMas191
