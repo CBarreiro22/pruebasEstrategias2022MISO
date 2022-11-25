6adceeaf5829bb5fd632b449dc5c0b518a899698
ELISA ANGULO <e.angulov@uniandes.edu.co>
2022-11-25 17:30:19 -0500

    Se crea una rama de contribucion para pruebas aleatrias

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:2368/ghost/#/signin'
+    baseUrl: 'http://localhost:3001/ghost/#/signin'
   },
   env: {
     email: 'c.barreiroh@uniandes.edu.co',
