323e0ce3447636220647edc43377484921430613
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2022-11-27 07:34:51 -0500

    add edit post

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001/ghost/#/signin'
+    baseUrl: 'http://localhost:2368/ghost/#/signin'
   },
   env: {
-    email: 'e.angulov@uniandes.edu.co',
-    password: 'Elymary-420@',
+    email: 'c.barreiroh@uniandes.edu.co',
+    password: 'Q123456789',
     mockarooUrl:'https://api.mockaroo.com/api/a1e06630',
     mockarooKey:'dcedeee0'
   }
